21e5e104163ed68558639dbec40361b7da4d2107
Lindsay Warr <iamalion@gmail.com>
2023-07-28 11:57:05 -0700

    All functionality working

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -40,6 +40,14 @@ namespace HairSalon.Controllers
             .FirstOrDefault(stylist => stylist.StylistId == id);
             return View(thisStylist);
         }
+        
+        public ActionResult Edit (int id)
+        {
+            Stylist thisStylist = _db.Stylists.FirstOrDefault
+            (stylist => stylist.StylistId == id);
+            return View(thisStylist);
+        }
+
         [HttpPost]
         public ActionResult Edit(Stylist stylist)
         {
