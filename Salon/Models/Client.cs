a8d50a06490dfba5fcfad7b93b12b31199312419
Lindsay Warr <iamalion@gmail.com>
2023-07-28 09:48:20 -0700

    Add Client.cs

---
+++ Salon/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace Salon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string Description { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; } 
+
+  }
+}
